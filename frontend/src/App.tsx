6aff7dea0f39bbd6382983b25411dc98f7ff2448
Rafaella Alves <rafaella.alves18@hotmail.com>
2022-07-14 22:25:53 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import NotificationButton from "./components/NotificationButton"
+// aqui fica todos componentes da tela
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
-    return (
-      <>
-      <h1>Rafaella Alves</h1> 
-      <NotificationButton/>
-      </>
-    ) 
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
